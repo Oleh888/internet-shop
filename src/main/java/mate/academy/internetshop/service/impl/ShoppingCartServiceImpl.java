6347d3a367632d3c58481499d4985d81ba17d57d
Oleh Kuzmenko <oleg24453@gmail.com>
2020-04-24 17:22:03 +0300

    corrected method getByUserId in ShoppingCartServiceImpl

--- src/main/java/mate/academy/internetshop/service/impl/ShoppingCartServiceImpl.java
+++ src/main/java/mate/academy/internetshop/service/impl/ShoppingCartServiceImpl.java
@@ -43,7 +43,7 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     public ShoppingCart getByUserId(Long userId) {
         return shoppingCartDao.getAll().stream()
                 .filter(shoppingCart -> shoppingCart.getUser().getUserId().equals(userId))
-                .findFirst().get();
+                .findFirst().orElseGet(() -> shoppingCartDao.create(new ShoppingCart(userService.get(userId))));
     }
 
     @Override
