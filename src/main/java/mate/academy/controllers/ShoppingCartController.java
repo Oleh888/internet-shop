55d9705f0a474fc1ea8707b8c4c48a778876c61d
Oleh888 <60585035+Oleh888@users.noreply.github.com>
2020-04-28 15:59:58 +0300

    Update src/main/java/mate/academy/controllers/ShoppingCartController.java
    Co-Authored-By: Pavel Kurilyuk <50627236+Tarabarnik@users.noreply.github.com>

--- src/main/java/mate/academy/controllers/ShoppingCartController.java
+++ src/main/java/mate/academy/controllers/ShoppingCartController.java
@@ -11,7 +11,7 @@ import mate.academy.model.ShoppingCart;
 import mate.academy.service.ProductService;
 import mate.academy.service.ShoppingCartService;
 
-public class ShoppingCartController extends HttpServlet {
+public class AddProductToShoppingCartController extends HttpServlet {
     private static final Long USER_ID = 1L;
     private static final Injector INJECTOR = Injector.getInstance("mate.academy");
     private final ShoppingCartService shoppingCartService =
