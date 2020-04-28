bed370d3bb00bb64cd0232021c72ea96f4fafa36
Oleh888 <60585035+Oleh888@users.noreply.github.com>
2020-04-28 14:04:00 +0300

    Update src/main/java/mate/academy/controllers/DeleteUsersController.java
    Co-Authored-By: Pavel Kurilyuk <50627236+Tarabarnik@users.noreply.github.com>

--- src/main/java/mate/academy/controllers/DeleteUsersController.java
+++ src/main/java/mate/academy/controllers/DeleteUsersController.java
@@ -9,7 +9,7 @@ import mate.academy.lib.Injector;
 import mate.academy.service.UserService;
 
 public class DeleteUsersController extends HttpServlet {
-    private static final Injector injector = Injector.getInstance("mate.academy");
+    private static final Injector INJECTOR = Injector.getInstance("mate.academy");
     private UserService userService = (UserService) injector.getInstance(UserService.class);
 
     @Override
