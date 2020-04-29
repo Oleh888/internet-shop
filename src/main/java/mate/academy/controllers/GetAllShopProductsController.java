ea7a4077aaa07e0292814723b36e4345be44e6dd
Oleh888 <60585035+Oleh888@users.noreply.github.com>
2020-04-29 19:40:51 +0300

    Update src/main/java/mate/academy/controllers/GetAllShopProductsController.java
    Co-Authored-By: Pavel Kurilyuk <50627236+Tarabarnik@users.noreply.github.com>

--- src/main/java/mate/academy/controllers/GetAllShopProductsController.java
+++ src/main/java/mate/academy/controllers/GetAllShopProductsController.java
@@ -10,7 +10,7 @@ import mate.academy.lib.Injector;
 import mate.academy.model.Product;
 import mate.academy.service.ProductService;
 
-public class GetAllShopProductsController extends HttpServlet {
+public class GetAllProductsController extends HttpServlet {
     private static final Injector INJECTOR = Injector.getInstance("mate.academy");
     private ProductService productService =
             (ProductService) INJECTOR.getInstance(ProductService.class);
