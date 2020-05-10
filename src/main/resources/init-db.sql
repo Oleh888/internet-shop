98e33eb6febfc07a2f3b95aea9fb14cdb4acd2d0
Oleh Kuzmenko <oleg24453@gmail.com>
2020-05-10 17:31:48 +0300

    corrected init-db.sql

--- src/main/resources/init-db.sql
+++ src/main/resources/init-db.sql
@@ -4,7 +4,5 @@ CREATE TABLE `internet_shop`.`products` (
     `product_id` BIGINT(11) NOT NULL AUTO_INCREMENT,
     `product_name` VARCHAR(225) NOT NULL,
     `price` DECIMAL(20) NOT NULL,
-    PRIMARY KEY (`product_id`),
-    UNIQUE INDEX `product_name_UNIQUE` (`product_name` ASC) VISIBLE,
-    UNIQUE INDEX `product_id_UNIQUE` (`product_id` ASC) VISIBLE
+    PRIMARY KEY (`product_id`)
     );
