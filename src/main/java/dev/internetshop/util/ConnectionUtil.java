11c72755faddc60e9f2bcc4bcde7e78ded684c7b
Oleh Kuzmenko <oleg24453@gmail.com>
2020-05-29 21:24:06 +0300

    hide own password from DB

--- src/main/java/dev/internetshop/util/ConnectionUtil.java
+++ src/main/java/dev/internetshop/util/ConnectionUtil.java
@@ -16,8 +16,8 @@ public class ConnectionUtil {
 
     public static Connection getConnection() {
         Properties dbProperties = new Properties();
-        dbProperties.put("user", "root");
-        dbProperties.put("password", "smilexe81");
+        dbProperties.put("user", "****");
+        dbProperties.put("password", "****");
         String url = "jdbc:mysql://localhost:3306/internet_shop?serverTimezone=UTC";
         try {
             return DriverManager.getConnection(url, dbProperties);
